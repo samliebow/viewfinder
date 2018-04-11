a5b89935faebd7b6c2536531769a953220568906
kguinto <klogold1@gmail.com>
2018-04-11 15:33:30 -0700

    Implemented simple textarea that inserts timestamps on enter and counts autonomy/horsepower traits

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+const moment = require('moment');
 
-const App = () => (
-    <div className='app'>This is a React app</div>
-);
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { autonomy: 0, horsepower: 0 };
+
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+  
+
+  handleKeyDown (event) {
+    if (!event.shiftKey && event.keyCode === 13) {
+      const text = event.target.value;
+
+      let autonomy = 0;
+      let horsepower = 0;
+
+      for (let i = 0; i + 1 < text.length; i++) {
+        if (text[i] === 'a') {
+          if (text[i+1] === '+') {
+            autonomy++;
+          } else if (text[i+1] === '-') {
+            autonomy--;
+          }
+        } else if (text[i] === 'h') {
+          if (text[i+1] === '+') {
+            horsepower++;
+          } else if (text[i+1] === '-') {
+            horsepower--;
+          }
+        }
+      }
+
+      this.setState({ autonomy, horsepower });
+
+      let cursorLocation = event.target.selectionEnd;
+
+      event.target.value = `${text.slice(0, cursorLocation)}\t\t[${moment().format('LTS')}]${text.slice(cursorLocation, text.length)}`;
+    }
+  }
+
+  render () {
+    return (
+      <div className='app'>
+        <span style={{ fontWeight: 'bold' }}>Interview Notes</span><br /><br />
+        Autonomy: {this.state.autonomy}&nbsp; Horsepower: {this.state.horsepower} 
+
+        <textarea
+          style={{ display: 'flex' }}
+          onKeyDown={this.handleKeyDown}
+        />
+      </div>
+  )};
+};
 
 export default App;
