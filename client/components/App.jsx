4164051d2c04b01ed538c26d26f60f62443328cd
kguinto <klogold1@gmail.com>
2018-04-27 13:45:14 -0700

    Moved components to individual component files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Notes from './Notes';
+import Prompt from './Prompt';
+import Setup from './Setup';
 
-const moment = require('moment');
-
-const Link = ({ link }) => (<a href={link}>{link}</a>);
+const { countHP, formatNotes } = require('./noteFns');
 
 class App extends Component {
   constructor (props) {
@@ -12,96 +12,94 @@ class App extends Component {
       text: '',
       autonomy: 0,
       horsepower: 0,
-      codestitch: '',
-      talkio: '',
-      zoom: ''
+      links: {
+        codestitch: '',
+        tlkio: '',
+        zoom: ''
+      },
+      show: {
+        setup: true,
+        prompt: true,
+        notes: true
+      },
+      promptSrc: ''
     };
 
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleCopy = this.handleKeyDown.bind(this);
     this.setRoom = this.setRoom.bind(this);
+    this.toggleShow = this.toggleShow.bind(this);
   }
   
 
   handleKeyDown (event) {
     if (!event.shiftKey && event.keyCode === 13) {
-      const text = event.target.value;
-
-      let autonomy = 0;
-      let horsepower = 0;
-
-      for (let i = 0; i + 1 < text.length; i++) {
-        if (text[i] === 'a' || text[i] === 'A') {
-          if (text[i+1] === '+') {
-            autonomy++;
-          } else if (text[i+1] === '-') {
-            autonomy--;
-          }
-        } else if (text[i] === 'h' || text[i] === 'H') {
-          if (text[i+1] === '+') {
-            horsepower++;
-          } else if (text[i+1] === '-') {
-            horsepower--;
-          }
-        }
-      }
-
-      this.setState({ autonomy, horsepower });
+      let text = event.target.value;
 
       let cursorLocation = event.target.selectionEnd;
+      let { autonomy, horsepower } = countHP(text);
 
-      event.target.value = `${text.slice(0, cursorLocation)}\t\t[${moment().format('LTS')}]${text.slice(cursorLocation, text.length)}`;
+      this.setState({ autonomy, horsepower });
+      event.target.value = formatNotes(text, cursorLocation);
     }
   }
 
   setRoom (room, value) {
-    this.setState({ [room]: value });
+    this.setState({
+      links: {
+        ...this.state.links,
+        [room]: value
+      }
+    });
+  }
+
+  toggleShow (section) {
+    this.setState({
+      show: Object.assign(this.state.show, {
+        [section]: !this.state.show[section]
+      })
+    });
   }
 
   render () {
     return (
-      <div className='app' style={{ padding: '2em' }}>
-        <span style={{ fontWeight: 'bold' }}>Interview Notes</span> - Autonomy: {this.state.autonomy}&nbsp; Horsepower: {this.state.horsepower} 
+      <div className='app' style={{ padding: '2em', height: 'calc(100vh - 4em)' }}>
 
-        <textarea
-          style={{
-            display: 'flex',
-            height: '10em',
-            width: 'calc(100% - .5em + 1px)'
-          }}
-          onKeyDown={this.handleKeyDown}
-          onChange={(event) => { this.setState({ text: event.target.value }) }}
+        <h1 style={{
+          fontSize: 24,
+          margin: '0 0 .5em 0',
+          padding: 0
+        }}>HR Interview Noter</h1>
+
+        <Setup
+          show={this.state.show.setup}
+          toggleShow={this.toggleShow}
+          setRoom={this.setRoom}
+          links={this.state.links}/>
+        <hr style={{ display: this.state.show.setup ? 'inline-block' : 'none'}} />
+
+        <Prompt
+          show={this.state.show.prompt}
+          toggleShow={this.toggleShow}
+          promptSrc={this.state.promptSrc}
+          setPromptSrc={
+            (promptSrc) => {
+              this.setState({ promptSrc });
+            }
+          }
         />
+        <hr style={{ display: this.state.show.prompt ? 'inline-block' : 'none'}} />
 
-        <CopyToClipboard text={ this.state.text }>
-          <button style={{ float: 'right', width: '6em' }}>📋</button>
-        </CopyToClipboard>
-
-        <ul>
-          <li><a href="https://drive.google.com/open?id=1yzyEviXgBXeAMRPoUVPutafJuSotGv9c72PgqHIU70o" target="_blank" >Prompts ⇗</a></li>
-          <li><a href="https://google.com/calendar" target="_blank">Calendar ⇗</a></li>
-          <li><a href="https://hackreactorcore.force.com/hackreactor" target="_blank">Salesforce ⇗</a></li>
-          <li><a href="https://codestitch.io" target="_blank">Codestitch ⇗</a></li>
-        </ul>
-        <hr />
-
-        <h4>On tlk.io:</h4>
-        <input placeholder="codestitch.io" onChange={(event) => {this.setRoom('codestitch', event.target.value)}}/>
-        <input placeholder="tlk.io" onChange={(event) => {this.setRoom('tlkio', event.target.value)}} />
-        <input placeholder="zoom" onChange={(event) => {this.setRoom('zoom', event.target.value)}} />
-
-        <div style={{ border: '1px solid black', padding: '.5em .5em .5em .5em' }}>
-          <pre style={{ whiteSpace: 'pre-wrap', margin: 0 }}>
-            Hi - if you are present, please input your name in the field below and press enter to join the chat. Then post a message so I know you’re here!<br />
-            -<br />
-            Hi! We’ll get started in just a few minutes. To get us started, can you please write your name and email in a comment in this codestitch.io pad? <Link link={this.state.codestitch} /> <br />
-            -<br />
-            (5 minutes no-show): Hi - It is currently time for your technical interview and we have been waiting for you. Please connect with us by going to the following URL: <Link link={this.state.tlkio} /><br />
-            -<br />
-            Here’s the link to our video room: <Link link={this.state.zoom} />. Once you have Zoom downloaded please click the link to join.
-
-          </pre>
-        </div>
+        <Notes
+          show={this.state.show.notes}
+          toggleShow={this.toggleShow}
+          text={this.state.text}
+          autonomy={this.state.autonomy}
+          horsepower={this.state.horsepower}
+          handleKeyDown={this.handleKeyDown}
+          handleNoteChange={(event) => { this.setState({ text: event.target.value }) }}
+        />    
+          
       </div>
   )};
 };
