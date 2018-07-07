b709f681bfbab5dd2a08787cdda38177a9157410
kguinto <klogold1@gmail.com>
2018-07-07 15:06:34 -0700

    Moved note taking state and methods to Notes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,38 +3,18 @@ import Notes from './Notes';
 import Prompt from './Prompt';
 import Setup from './Setup';
 
-const { countHP, formatNotes } = require('./noteFns');
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
-      autonomy: 0,
-      horsepower: 0,
       show: {
-        prompt: true,
-        notes: true
+        prompt: true
       },
       promptSrc: ''
     };
 
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleCopy = this.handleKeyDown.bind(this);
     this.setRoom = this.setRoom.bind(this);
     this.toggleShow = this.toggleShow.bind(this);
-  }
-
-  handleKeyDown(event) {
-    if (!event.shiftKey && event.keyCode === 13) {
-      let text = event.target.value;
-
-      let cursorLocation = event.target.selectionEnd;
-      let { autonomy, horsepower } = countHP(text);
-
-      this.setState({ autonomy, horsepower });
-      event.target.value = formatNotes(text, cursorLocation);
-    }
   }
 
   setRoom(room, value) {
@@ -84,17 +64,7 @@ class App extends Component {
           style={{ display: this.state.show.prompt ? 'inline-block' : 'none' }}
         />
 
-        <Notes
-          show={this.state.show.notes}
-          toggleShow={this.toggleShow}
-          text={this.state.text}
-          autonomy={this.state.autonomy}
-          horsepower={this.state.horsepower}
-          handleKeyDown={this.handleKeyDown}
-          handleNoteChange={event => {
-            this.setState({ text: event.target.value });
-          }}
-        />
+        <Notes />
       </div>
     );
   }
--- client/components/Notes.jsx
+++ client/components/Notes.jsx
@@ -1,29 +1,64 @@
-import React from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import React, { Component } from 'react';
 import SectionTitle from './SectionTitle';
+import { countHP, formatNotes } from './noteFns';
 
-const Notes = ({ show, toggleShow, text, horsepower, autonomy, handleKeyDown, handleNoteChange }) => (
-  <div className='Notes' style={{ marginBottom: '2em' }}>
-   <SectionTitle title='Notes' sectionName='notes' toggleShow={toggleShow} />
+// ({ show, toggleShow, text, horsepower, autonomy, handleKeyDown, handleNoteChange })
+class Notes extends Component {
+  state = {
+    text: '',
+    autonomy: 0,
+    horsepower: 0,
+    show: true
+  };
 
-    <span>
-    - Autonomy: {autonomy}&nbsp; Horsepower: {horsepower} 
+  handleKeyDown = event => {
+    if (!event.shiftKey && event.keyCode === 13) {
+      let text = event.target.value;
 
-    <textarea
-      style={{
-        display: 'flex',
-        height: '10em',
-        width: 'calc(100% - .5em + 1px)',
-      }}
-      onKeyDown={handleKeyDown}
-      onChange={handleNoteChange}
-    />
+      let cursorLocation = event.target.selectionEnd;
+      let { autonomy, horsepower } = countHP(text);
 
-    <CopyToClipboard text={ text }>
-      <button style={{ float: 'right', width: '6em' }}>📋</button>
-    </CopyToClipboard>
-    </span>
-  </div>
-);
+      this.setState({ autonomy, horsepower });
+      event.target.value = formatNotes(text, cursorLocation);
+    }
+  };
+
+  handleNoteChange = event => {
+    this.setState({ text: event.target.value });
+  };
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  render() {
+    const { text, horsepower, autonomy, show } = this.state;
+
+    return (
+      <div className="Notes" style={{ marginBottom: '2em' }}>
+        <SectionTitle
+          title="Notes"
+          sectionName="notes"
+          toggleShow={this.toggleShow}
+        />
+        <span style={{ display: this.state.show ? 'inline' : 'none' }}>
+          - Autonomy: {autonomy}&nbsp; Horsepower: {horsepower}
+          <textarea
+            style={{
+              display: 'flex',
+              height: '10em',
+              width: 'calc(100% - .5em + 1px)'
+            }}
+            onKeyDown={this.handleKeyDown}
+            onChange={this.handleNoteChange}
+          />
+        </span>
+        <div style={{ display: this.state.show ? 'none' : 'inline-block' }}>
+          &nbsp;...{' '}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Notes;
