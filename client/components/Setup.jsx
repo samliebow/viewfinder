fbdb5cf3d309df5de00dacbfb4b48b915cf373a2
Sam Liebow <sam.liebow@gmail.com>
2018-07-17 15:46:33 -0700

    Add reading next interview info from calendar on load

--- client/components/Setup.jsx
+++ client/components/Setup.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
+import apiKey from '../../apiKey.js';
 import SectionTitle from './SectionTitle';
 import TlkioScript from './TlkioScript';
 import LinkRenderer from './LinkRenderer';
@@ -18,12 +19,54 @@ const Input = ({ name, setter }) => (
 class Setup extends Component {
   state = {
     candidateName: '',
+    candidateEmail: '',
     rooms: {
       tlkio: '',
       codestitch: '',
       zoom: ''
     },
     show: true
+  };
+
+  componentDidMount() {
+    gapi.load('client:auth2', async () => {
+      await gapi.client.init({
+        apiKey,
+        // discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+        clientId: '695004460995-m8hbtlfkktf3d6opnafeer5j0dsbmn72.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/calendar',
+      });
+      const GoogleAuth = gapi.auth2.getAuthInstance();
+      GoogleAuth.isSignedIn.listen(this.makeAPICall);
+      if (GoogleAuth.isSignedIn.get()) {
+        this.makeAPICall();
+      } else {
+        GoogleAuth.signIn();
+      }
+    });
+  }
+
+  makeAPICall = async () => {
+    const calendarsData = await gapi.client.request({
+      path: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+    });
+    const calendarId = calendarsData.result.items.filter(
+      ({ summary }) => summary.includes('HiR'))[0].id;
+    const interviewsData = await gapi.client.request({
+      path: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
+      params: {
+        singleEvents: true,
+        orderBy: 'startTime',
+        q: '#Interview Online with',
+        timeMin: new Date((new Date() - 6e5)).toISOString(), // 10 minutes ago
+      },
+    });
+    const soonestInterviewData = interviewsData.result.items[0];
+    const soonestInterviewDetails = soonestInterviewData.description.split('\n').slice(0, 3);
+    const [candidateName, candidateEmail, tlkioLink] = soonestInterviewDetails.map(el => el.split(': ')[1]);
+    this.setName(candidateName);
+    this.setState({ candidateEmail });
+    this.setRoom(tlkioLink, 'tlkio');
   };
 
   setName = candidateName => {
