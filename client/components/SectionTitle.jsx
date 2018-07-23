a1efe229300ca334c9f0343a5732e8bb7bd08e4c
Sam Liebow <sam.liebow@gmail.com>
2018-07-22 21:45:58 -0700

    Make collapse functionality a little more apparent

--- client/components/SectionTitle.jsx
+++ client/components/SectionTitle.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const SectionTitle = ({ title, sectionName, toggleShow }) => (
-  <strong onClick={() => { toggleShow(sectionName) }} style={{ display: 'inline-block', cursor: 'pointer' }}>{title}</strong>
+  <strong 
+    onClick={() => { toggleShow(sectionName) }} 
+    style={{
+      display: 'inline-block',
+      cursor: 'pointer',
+      'text-decoration': 'underline',
+      color: '#0000EE'
+    }}
+  >
+    {title}
+  </strong>
 );
 
 export default SectionTitle;
