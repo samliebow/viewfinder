956560f85220245972c70db7b89b36434f1bd3be
Sam Liebow <sam.liebow@gmail.com>
2018-07-28 16:42:07 -0700

    Remove no-longer-needed URLs

--- client/components/Steps.jsx
+++ client/components/Steps.jsx
@@ -1,7 +1,5 @@
 const decisionsUrl = 'https://docs.google.com/spreadsheets/d/1ObVQGqm894fzjeM5vcG2qysYKDng4g8rtgyIwS3vJh8/edit#gid=391982378';
 const codestitchUrl = 'https://codestitch.io';
-const myDriveUrl = 'https://drive.google.com/drive/my-drive';
-const monthFoldersUrl = 'https://drive.google.com/drive/folders/0B5_RJCdGH93GdW1fMWMzQlg3VEE';
 const tiDecisionsUrl = 'https://goo.gl/forms/IODn7sw3jtpiUq2n1';
 const tiWorkflowUrl = 'https://docs.google.com/document/d/18AJkthUSgu40QUYwQNdQ3B23SIFMVSU5HDr_5bVaCws/edit';
 
