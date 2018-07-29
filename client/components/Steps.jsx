022f03ebd48a2b3d59d2e6a9af29bdadd099cc1c
Sam Liebow <sam.liebow@gmail.com>
2018-07-29 11:43:16 -0700

    Make display of past interview records more intuitive

--- client/components/Steps.jsx
+++ client/components/Steps.jsx
@@ -3,32 +3,42 @@ const codestitchUrl = 'https://codestitch.io';
 const tiDecisionsUrl = 'https://goo.gl/forms/IODn7sw3jtpiUq2n1';
 const tiWorkflowUrl = 'https://docs.google.com/document/d/18AJkthUSgu40QUYwQNdQ3B23SIFMVSU5HDr_5bVaCws/edit';
 
-const Steps = ({ candidateName, candidateEmail, currentDate, tlkio, staticTiRows, liveTiRows, suggestedPrompt, copyPrompt, }) => (
+const Steps = ({ candidateName, candidateEmail, currentDate, tlkio, staticTiRows, liveTiRows, suggestedPrompt, copyPrompt, }) => {
+  const pastRecordsAvailable = !!(staticTiRows && liveTiRows);
+  let allTiRows, isFirstInterview;
+  if (pastRecordsAvailable) {
+    allTiRows = staticTiRows.concat(liveTiRows);
+    isFirstInterview = !allTiRows.length;
+  }
+
+  const suggestedPromptLine = (<span>
+    <a href="#" id={suggestedPrompt} onClick={copyPrompt}>
+      Use {suggestedPrompt}
+    </a>, or choose a prompt below. It will save to this month's folder.
+  </span>);
+  const candidateFirst = candidateName && candidateName.split(' ')[0];
+
+  return (
   <ol>
-    <li>Records for {candidateEmail || 'the candidate\'s email'} in the <a href={decisionsUrl} target="_blank">Technical Interview Decisions</a> spreadsheet:</li>
-      {staticTiRows ?
-        <ul>Interviews scheduled by {candidateName.split(' ')[0] || 'candidate'} before 6/27/18:
-          {staticTiRows.length ? staticTiRows.map(str => <li key={str}>{str}</li>) : ' None.'}
-        </ul>
-      : null }
-      {liveTiRows ?
-        <ul>Interviews scheduled by {candidateName.split(' ')[0] || 'candidate'} since 6/27/18:
-          {liveTiRows.length ? liveTiRows.map(str => <li key={str}>{str}</li>) : ' None.'}
-        </ul>
-      : null}
-      {suggestedPrompt ? 
-        <ul>It looks like they haven't used {suggestedPrompt}.&nbsp; 
-          <a href="#" id={suggestedPrompt} onClick={copyPrompt}>
-              Use {suggestedPrompt}
-          </a>, or choose a prompt below. It will save to this month's folder.
-        </ul>
-        : null }
+    {pastRecordsAvailable ?
+      (isFirstInterview ?
+        <li>It's {candidateFirst}'s first scheduled interview! {suggestedPromptLine}</li> :
+        <li>All previous interviews by {candidateFirst} in the <a href={decisionsUrl} target="_blank">Technical Interview Decisions</a> spreadsheet:
+          <ul>
+            {allTiRows.map(str => <li key={str}>{str}</li>)}
+            <li>{suggestedPromptLine}</li>
+          </ul>
+        </li>
+      ) :
+      <li>Wait just a moment while we look up previous interview records...</li>
+    }
     <li>Open up a <a href={codestitchUrl} target="_blank">Codestitch</a> pad and paste the URL below.</li>
     <li>Schedule a Zoom call named <i>{candidateName || 'FIRSTNAME LASTNAME'} - {currentDate}</i> and paste the join link below.</li>
     <li>Go to <a href={tlkio} target="_blank">the tlk.io link</a> and conduct the interview using the script snippets below.</li>
     <li>Fill out the <a href={tiDecisionsUrl} target="_blank">Technical Interview Decisions Form</a>.</li>
     <li>If you have any questions, reference the <a href={tiWorkflowUrl} target="_blank">TI Workflow</a>.</li>
   </ol>
-);
+  );
+};
 
 export default Steps;
