22980df1896599c6ffa41d58e960e5e460dda38e
Sam Liebow <sam.liebow@gmail.com>
2018-08-04 19:15:46 -0700

    Add server endpoint: sends Codestitch url, then generates new one

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const app = express();
+const fs = require('fs');
+const path = require('path');
+const codestitchGen = require('./codestitchGen.js');
 
+const app = express();
 const PORT = process.env.port || 3033;
 
 app.use(express.static('client/dist'));
+
+app.get('/codestitch', (req, res) => {
+  const url = fs.readFileSync(path.join(__dirname, 'codestitchUrl.txt'), 'utf8');
+  res.send(url);
+  codestitchGen();
+});
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT);
