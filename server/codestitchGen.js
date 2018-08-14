c03ad1a5ed661e9505abf8bb99ab67c0d89d1045
Sam Liebow <sam.liebow@gmail.com>
2018-08-13 21:17:19 -0700

    Simplify and debug Codestitch URL generation

--- server/codestitchGen.js
+++ server/codestitchGen.js
@@ -1,44 +1,34 @@
 // Derived from https://github.com/kjng/codestitch-creator - which another HiR made!
 const fs = require('fs');
 const path = require('path');
-const nightmare = require('nightmare')();
+const Nightmare = require('nightmare');
 const { email, password } = require('./codestitchCredentials.js');
 
 const generateUrl = async () => {
-  let wait = 1000;
-  let url;
-  let atLogin = false;
-  let atPad = false;
-  while (!atLogin && wait <= 3000) {
-    url = await nightmare
-      .goto('https://codestitch.io/auth/sign_in')
-      .type('#user_email', email)
-      .type('#user_password', password)
-      .click('input[value="Sign in"]')
-      .wait(wait)
-      .url();
-    wait += 1000;
-    atLogin = url === 'https://codestitch.io/pads';
-  }
-  if (!atLogin) {
+  const nightmare = Nightmare();
+  let url = await nightmare
+    .goto('https://codestitch.io/auth/sign_in')
+    .type('#user_email', email)
+    .type('#user_password', password)
+    .click('input[value="Sign in"]')
+    .wait('img', 2000) // Hacky way to wait until done logging in
+    .url();
+  if (url !== 'https://codestitch.io/pads') {
     throw new Error(`Couldn't log in.`);
   }
 
-  wait = 500;
-  while (!atPad && wait < 3000) {
-    url = await nightmare
-      .goto('https://codestitch.io/pads/new')
-      .wait(wait)
-      .url();
-    wait += 1500;
-    atPad = !!url.match(/[a-z0-9]{8}$/)
-  }
-  if (!atPad) {
+  url = await nightmare
+    .goto('https://codestitch.io/pads/new')
+    .wait('#run', 2000) // Hacky way to wait until pad created
+    .url(); 
+  if (!url.match(/[a-z0-9]{8}$/)) {
     throw new Error(`Couldn't get to pad.`);
   }
 
+  // .then after .end is required
+  nightmare.end(() => 'Headless browser stopped.').then(console.log.bind(console));
   fs.writeFile(path.join(__dirname, 'codestitchUrl.txt'), url, 
-    () => console.log('New Codestitch URL generated.'));
+    () => console.log(`New Codestitch URL generated for next interview: ${url}.`));
 };
 
 module.exports = generateUrl;
